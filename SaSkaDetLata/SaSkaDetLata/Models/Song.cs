1aaa8f86f9f357d8ec06005764734902e926014d
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-09-22 17:53:01 +0200

    Stylecop

--- SaSkaDetLata/SaSkaDetLata/Models/Song.cs
+++ SaSkaDetLata/SaSkaDetLata/Models/Song.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SaSkaDetLata.Models
+﻿namespace SaSkaDetLata.Models
 {
+    using System.Collections.Generic;
+
     public class Song
     {
         public List<Panel> Panels { get; set; }
+
         public string ArtistName { get; set; }
+
         public string SongName { get; set; }
+
         public string Lyrics { get; set; }
 
         public void Populate()
